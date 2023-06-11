99f0e1ea57c7bbf01efae08e5233869e5f5079d6
AndreiPetoukhov1986 <oziglena@mail.ru>
2023-06-11 22:22:10 +0300

    First Commit

---
+++ src/main/java/com/example/kursrabota/KursRabota2Application.java
@@ -0,0 +1,13 @@
+package com.example.kursrabota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursRabota2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursRabota2Application.class, args);
+    }
+
+}
